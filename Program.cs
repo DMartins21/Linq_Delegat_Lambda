93354113b6b5f2fc311c95ce9a0fc55793275a17
Davi <davi_martins2009@outlook.com>
2022-12-08 23:42:01 -0300

    Delegate

--- Program.cs
+++ Program.cs
@@ -5,30 +5,14 @@ using Linq_Lambda.Services;
 using Linq_Lambda.Entities;
 
 
-
-List<Product> list = new();
-delegate double BinaryNumericOperation(double n1, double n2);
-list.Add(new Product("TV", 900.00));
-list.Add(new Product("Notebook", 1200.00));
-list.Add(new Product("Tablet", 450.00));
-
-Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-
-list.Sort(comp);
-
-foreach (Product item in list)
-{
-    Console.WriteLine(item);
-}
-
-
-
 double a = 10;
 double b = 12;
 
 BinaryNumericOperation op = CalculationService.Sum;
 
-
 double result = op(a, b);
 
 Console.WriteLine(result);
+
+delegate double BinaryNumericOperation(double n1, double n2);
+
