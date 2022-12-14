cc18c260cd1a7ecaa36586f8461357848a76db5e
Davi <davi_martins2009@outlook.com>
2022-12-14 14:42:54 -0300

    Introducao ao Linq

--- Program.cs
+++ Program.cs
@@ -1,38 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Linq_Lambda.Services;
-using Linq_Lambda.Entities;
-using System.Collections.Generic;
 using System.Linq;
 
-List<Product> list = new();
+int[] numbers = new int[] { 2, 3, 4, 5 };
 
-list.Add(new Product("TV", 900.00M));
-list.Add(new Product("Mouse", 50.00M));
-list.Add(new Product("Notebook", 1200.00M));
-list.Add(new Product("Tablet", 450.00M));
-
-//list.RemoveAll(ProductTest);
-
-//list.ForEach(UpdatePrice);
-
-List<string> result = list.Select(NameUpper).ToList();
-
-foreach (string item in result)
+IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+foreach(var r in result)
 {
-    Console.WriteLine(item);
-}
-
-static bool ProductTest(Product p)
-{
-    return p.Price >= 100.00M;
-}
-
-static void UpdatePrice(Product p)
-{
-    p.Price += p.Price * 0.1M;
-}
-
-static string NameUpper(Product p)
-{
-   return p.Name.ToUpper();
+    Console.WriteLine(r);
 }
